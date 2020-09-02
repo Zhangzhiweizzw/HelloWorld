68a6e58017d15f9a9e52c024b474ef92dda414dd
Zhangzhiweizzw <42959112+Zhangzhiweizzw@users.noreply.github.com>
2020-09-02 13:59:07 +0800

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello!");
-        System.out.println("This is Version 2.0.");
+        System.out.println("This is Version 3.0.");
     }
 }
