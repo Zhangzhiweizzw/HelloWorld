9cf66fadfdfe50492b69d3d287ff6155d1ab8720
Zhangzhiweizzw <zhangzhiweizzw@foxmail.com>
2020-09-02 14:26:16 +0800

    Give Local Tag

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
         System.out.println("Hello!");
         System.out.println("This is Version 2.0.");
         System.out.println("I know it's version 3.0");
+
+        System.out.println("Local");
     }
 }
