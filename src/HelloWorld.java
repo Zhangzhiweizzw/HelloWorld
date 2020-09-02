795336ebda907a494566f3b5b76565c4a9e19513
Zhangzhiweizzw <zhangzhiweizzw@foxmail.com>
2020-09-02 14:04:32 +0800

    local version 3.0

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("This is Version 2.0.");
+        System.out.println("I know it's version 3.0");
     }
 }
