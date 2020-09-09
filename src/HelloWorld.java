c110d08d616066df5be56fce552ea786f1bb3a5b
Zhangzhiweizzw <42959112+Zhangzhiweizzw@users.noreply.github.com>
2020-09-09 09:35:25 +0800

    Test for undo

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello!");
-        System.out.println("This is Version 2.0.");
-        System.out.println("I know it's version 3.0");
-
-        System.out.println("Remote");
-        System.out.println("Local");
+        System.out.print("This a test for \"undo\" operation.");
     }
 }
